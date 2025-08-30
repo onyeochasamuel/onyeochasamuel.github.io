a166d0f36ded1bf1a988a721af0d9cfc75af45e0
onyeochasamuel <125794484+onyeochasamuel@users.noreply.github.com>
2025-08-30 17:34:50 +0100

    Add files via upload

---
+++ tawk.js
@@ -0,0 +1,11 @@
+// Start of Tawk.to Script
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/68b1c10bbe643f19218493b1/1j3r5k5hm';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+// End of Tawk.to Script
